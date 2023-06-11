5886a0378ae34932b0dd330e3f65db5a5324a101
Ahmed Fahim <afahim03@yahoo.com>
2023-06-11 16:10:07 +0600

    Add files for 03-orchestration homework(prefect)

--- 03-orchestration/3.4/orchestrate.py
+++ 03-orchestration/3.4/orchestrate.py
@@ -9,9 +9,10 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
 
 
-@task(retries=3, retry_delay_seconds=2)
+@task(retries=3, retry_delay_seconds=2, name="Read taxi data")
 def read_data(filename: str) -> pd.DataFrame:
     """Read data into DataFrame"""
     df = pd.read_parquet(filename)
@@ -106,13 +107,18 @@ def train_best_model(
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+        create_markdown_artifact(
+            key="val-rmse",
+            markdown=f"Val_rmse: {rmse:.2f}",
+            description="Validation RMSE",
+        )
     return None
 
 
 @flow
 def main_flow(
-    train_path: str = "./data/green_tripdata_2021-01.parquet",
-    val_path: str = "./data/green_tripdata_2021-02.parquet",
+    train_path: str = "./data/green_tripdata_2023-01.parquet",
+    val_path: str = "./data/green_tripdata_2023-02.parquet",
 ) -> None:
     """The main training pipeline"""
 
@@ -133,3 +139,4 @@ def main_flow(
 
 if __name__ == "__main__":
     main_flow()
+
